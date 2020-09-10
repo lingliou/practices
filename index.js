1c83cca2823e6b828fc0c305c7599fdc3080b67e
Lily <lilyau0112@gmail.com>
2020-09-10 16:03:29 +0800

    fliter

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const urlBase = "https://api.punkapi.com/v2/beers";
+const filterABV = document.getElementById('filterABV');
+let optionABV ='';
+
+// fliters
+fliterABV.addEventListener('change', e =>{
+    const value =e.target.value;
+
+    switch (value){
+        case "all":
+            optionABV="";
+            break
+        case "weak":
+            optionABV="abv_lt=4.6";
+            break
+        case "medium":
+            optionABV="abv_gt=4.5&abv_lt=7.6";
+            break
+        case "medium":
+            optionABV="abv_gt=7.5";
+            break
+    }
+});
 
 async function getBeers() {
+    const url = urlBase + "?" +optionABV;
     // fetch
-    const beerPromise = await fetch(urlBase);
+    const beerPromise = await fetch(url);
     const beers = await beerPromise.json();
 
     // render data
@@ -30,7 +53,7 @@ async function getBeers() {
                 </div>
             </div>
         </div>
-       `; 
+       `;
     });
 
     beersDiv.innerHTML = beerHtml;

300a12bc789e1506573dca429182b5b1972123b9
Lily <lilyau0112@gmail.com>
2020-09-10 15:13:26 +0800

    fetch data from punk api

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// javascript
+const urlBase = "https://api.punkapi.com/v2/beers";
+
+async function getBeers() {
+    // fetch
+    const beerPromise = await fetch(urlBase);
+    const beers = await beerPromise.json();
+
+    // render data
+    const beersDiv = document.querySelector('.beers');
+
+    let beerHtml = "";
+
+    beers.forEach(beer => {
+       beerHtml += `<h3>${beer.name}</h3>`;
+    });
+
+    beersDiv.innerHTML = beerHtml;
+}
+
+// initial get
+getBeers();

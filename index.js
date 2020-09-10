dab249545513b28d07aba908db33181b3082b98a
Lily <lilyau0112@gmail.com>
2020-09-10 15:20:10 +0800

    fetch data - beer.name, beer.image_url, beer.abv, beer.ibu

--- index.js
+++ index.js
@@ -11,7 +11,18 @@ async function getBeers() {
     let beerHtml = "";
 
     beers.forEach(beer => {
-       beerHtml += `<h3>${beer.name}</h3>`;
+       beerHtml += `
+        <div class='beer-wrapper card'>
+            <div class='beer'>
+                <img class='beer__img' src="${beer.image_url}">
+                <h3>${beer.name}</h3>
+                <span class='beer__info'>
+                    <span>ABV: ${beer.abv}%</span>
+                    <span>IBU: ${beer.ibu}</span>
+                </span>
+            </div>
+        </div>
+       `;
     });
 
     beersDiv.innerHTML = beerHtml;

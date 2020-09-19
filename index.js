f5c80527f8cea063f38e90ecaaea91032fac66b9
Lily <lilyau0112@gmail.com>
2020-09-19 20:42:01 +0800

    fix data Inconsistency issue

--- index.js
+++ index.js
@@ -82,6 +82,7 @@ async function getBeers() {
     const beersDiv = document.querySelector('.beers');
     
     let beerHtml = "";
+    const genericBottle = 'https://cdn.pixabay.com/photo/2014/12/22/00/04/bottle-576717_960_720.png';
       
     // Fill in the blanks with the rest of the data
     
@@ -89,7 +90,7 @@ async function getBeers() {
        beerHtml += `
         <div class='beer-wrapper card'>
             <div class='beer'>
-                <img class='beer__img' src="${beer.image_url}">
+                <img class='beer__img' src="${beer.image_url ? beer.image_url : genericBottle}">
                 <h3>${beer.name}</h3>
                 <span class='beer__info'>
                     <span>ABV: ${beer.abv}%</span>
